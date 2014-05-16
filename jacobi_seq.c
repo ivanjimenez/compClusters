ca46e756a98be8b8a052d8db3076b04e83f57818
Usuario 4 para practicas diego 2013-2014 <ivan.jimenezster@gmail.com>
2014-05-16 23:21:47 +0200

    first

---
+++ jacobi_seq.c
@@ -0,0 +1,69 @@
+/* Algoritmo de jacobi 
+ *
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "memoryfun.h"
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+	
+	int result;
+	int i,j,l;
+	int m,n,k,lda,ldb;
+	double *A,*B;
+
+	clock_t inicio, fin;
+	double  duration;
+	
+	/* Comprobación número de argumentos correctos. Se pasaran m n k */
+	if (argc!=2)
+	   {
+	   printf("Error de Sintaxis. Uso: jacobi_seq n \n");
+	   exit(1);
+	   }
+
+	/* Lectura de parametros de entrada */
+	n=atoi(argv[1]);
+
+	/* Dimensionado de las matrices, utilizando funciones propias */
+	lda=n; ldb=n; ldc=n; 
+	A=dmatrix(n,n); B=dmatrix(n,n); C=dmatrix(n,n);
+
+	/* Relleno de las matrices con valores aleatorios. Uso de macro propia */
+
+	//matrixA 
+
+		for(i=0;i<n;i++)
+			for(j=0;j<n;j++)
+				M(A,i,j,lda) = rand() % 20;
+	
+	//matrizB:: Está será nuestra matriz transpuesta
+
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			M(B,i,j,ldb) = rand() % 20;
+	
+				
+	
+// Computa la operación: cblas.dot <- X^T*Y
+
+inicio = clock();
+
+for(i=0;i<n;i++)
+	for(j=0;j<n;j++)
+		M(C,i,j,ldc) = cblas_ddot(n,A+i,lda,B+j*ldb,1);
+		 
+                  
+fin = clock();
+duration = (double)(fin - inicio) / CLOCKS_PER_SEC;
+printf("%d %d\n", n, result);
+printf("x^T*y: %2.5f segundos\n", duration );
+
+
+}
